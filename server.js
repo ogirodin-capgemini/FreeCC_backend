1c2268d4738d6d96b9e279165b9b9021a56d9c95
Olivier Girodin <ogirodin@gmail.com>
2022-01-16 12:18:21 +0100

    stylesheet middleware added

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+app.use('/public', express.static('public'));
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
