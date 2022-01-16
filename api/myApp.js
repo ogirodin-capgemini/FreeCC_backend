c8a23985780bd64ae9a9588da53becc60229d301
ogirodin <ogirodin@hotmail.com>
2022-01-16 23:45:53 +0000

    findPersonById implemented

--- api/myApp.js
+++ api/myApp.js
@@ -51,11 +51,14 @@ const findOneByFood = (food, done) => {
   Person.findOne({favoriteFoods: food}, (err, data) => {
   if (err) {return done(err);}
   done(null, data);
-  })
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+  if (err) {return done(err);}
+  done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
