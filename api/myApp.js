34308bd279bcb5c92c37c0512e0d5d81b0295330
ogirodin <ogirodin@hotmail.com>
2022-01-16 18:54:11 +0000

    name route - sending {name: ...}

--- api/myApp.js
+++ api/myApp.js
@@ -19,6 +19,10 @@ app.get('/name', (req, res) => {
   res.send({name: `${req.query.first} ${req.query.last}`});
 });
 
+app.post('/name', (req, res) => {
+  res.send({name: `${req.body.first} ${req.body.last}`});
+});
+
 app.get('/:word/echo', (req, res) => {
   res.json({"echo": req.params.word});
 });
