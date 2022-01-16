d12a5f55d2af8b27ba636c36606b9b85e78f54a5
Olivier Girodin <ogirodin@gmail.com>
2022-01-16 17:54:29 +0100

    time middleware - creating a route chained with middleware to handle synchronous operations

--- api/myApp.js
+++ api/myApp.js
@@ -7,12 +7,19 @@ console.log('Hello World');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
-})
+});
 
 app.get('/json', (req, res) => {
   const hello = process.env.MESSAGE_STYLE === 'uppercase' ? "Hello json".toUpperCase() : "Hello json";
   res.json({"message": hello});
-})
+});
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();  // Hypothetical synchronous operation
+  next();
+}, function(req, res) {
+  res.send({time: req.time});
+});
 
 
 
